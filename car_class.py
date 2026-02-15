365a57d625aa64be51b7fef7b29dd79681cc5062
dilenliyanage-art <dilenliyanage@gmail.com>
2026-02-15 14:13:20 -0600

    Add files via upload

---
+++ car_class.py
@@ -0,0 +1,8 @@
+class car:
+    def __init__(self,max_speed,milege):
+        self.max_speed = max_speed
+        self.milege = milege
+modelx = car(18,30)
+print("modles max speed is ",modelx.max_speed)
+print("modles milege is ",modelx.milege)
+
